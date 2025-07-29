877660a52f7494458cd0ed0f8d2e6749b3fc0210
Vybekid <felobitto@gmail.com>
2025-07-29 23:44:45 +0300

    update

---
+++ chant.py
@@ -0,0 +1,3 @@
+statement = "Krishna I can feel your saving from deep within"
+Chant = "Hare Krishna Hare Krishna Krishna Krishna Hare Hare Hare Rama Hare Rama Rama Rama Hare Hare"
+print(statement, "thats Why I say: ", Chant)
